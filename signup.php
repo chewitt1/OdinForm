ed62927d34b7f1a7793455e6916d88ce5891d61e
Carrima Hewitt <chewitt1@ufl.edu>
2023-05-24 23:50:26 -0400

    Update PHP

--- signup.php
+++ signup.php
@@ -1,5 +1,11 @@
 <?php
-    ECHO ("First Name: " . $_POST["fname"] . "<br>");
-    ECHO ("Email: " . $_POST["email"] . "<br>");
-    ECHO ("Password: " . $_POST["pass"] . "<br>");
+    ECHO ("First Name: " .  htmlspecialchars($_POST["fname"]) . "<br>");
+    if(htmlspecialchars($_POST["fname"]) != null){
+        ECHO ("Last Name: " .  htmlspecialchars($_POST["lname"]) . "<br>");
+    }
+    ECHO ("Email: " . htmlspecialchars($_POST["email"]) . "<br>");
+    if(htmlspecialchars($_POST["fname"]) != null){
+        ECHO ("Phone Number: " .  htmlspecialchars($_POST["phone"]) . "<br>");
+    }
+    ECHO ("Password: " . htmlspecialchars($_POST["pass"]) . "<br>");
 ?>
